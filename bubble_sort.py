6f5b910d5f0b1229aef01928b09820835f2603f5
asemg <35200958+asemg@users.noreply.github.com>
2018-07-28 11:36:50 -0400

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,10 +2,9 @@ def bubble_sort(arr):
 	is_sorted = False
 	while not is_sorted:
 		is_sorted = True
-		
 		for k in range(len(arr)):
 			if k < len(arr) - 1:
 				if arr[k] > arr[k+1]:
 					is_sorted = False
-				    arr[k], arr[k+1] = arr[k+1], arr[k]
+					arr[k], arr[k+1] = arr[k+1], arr[k]
 	return arr
